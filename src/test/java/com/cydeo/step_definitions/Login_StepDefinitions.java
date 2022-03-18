2158d1a45fdaa0f264d5e45dd4cd93777d85ccf9
Zarak A <alamz523@gmail.com>
2022-03-18 15:06:51 -0500

    day 14 login.feature

--- src/test/java/com/cydeo/step_definitions/Login_StepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/Login_StepDefinitions.java
@@ -1,6 +1,7 @@
 package com.cydeo.step_definitions;
 
 import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
@@ -40,5 +41,10 @@ public class Login_StepDefinitions {
     @And("user enters admin password")
     public void userEntersAdminPassword() {
         System.out.println("User enters admin password");
+    }
+
+    @Given("user is on the library login page")
+    public void userIsOnTheLibraryLoginPage() {
+        System.out.println("User is on the Library login page");
     }
 }

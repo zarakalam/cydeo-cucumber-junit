fcedbd12f00acc6d5a909626c3cb6f72a05e3519
Zarak A <alamz523@gmail.com>
2022-03-21 13:46:44 -0500

    day 17 Web_Tables_StepDefinitions.java

--- src/test/java/com/cydeo/pages/DropDownsPage.java
+++ src/test/java/com/cydeo/pages/DropDownsPage.java
@@ -1,6 +1,8 @@
 package com.cydeo.pages;
 
 import com.cydeo.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class DropDownsPage {
@@ -9,7 +11,8 @@ public class DropDownsPage {
         PageFactory.initElements(Driver.getDriver(), this);
     }
 
-
+    @FindBy(xpath = "//*[@id=\"month\"]")
+    public WebElement monthDropDown;
 
 }
 
